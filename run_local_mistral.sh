0f9e6740a6e47e84e80a26a9612f08c31fac8a36
Daniel Kim <dhjkim2001@gmail.com>
2025-06-13 14:46:10 +0100

    Added script for queueing

--- run_local_mistral.sh
+++ run_local_mistral.sh
@@ -1,3 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N eval_local_mistral
+#$ -cwd
+#$ -l h_rt=12:00:00
+#$ -l h_vmem=512G
+#$ -q gpu
+#$ -l gpu=1
 
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load anaconda
+
+conda activate diss
+
+# Run the program
 python src/pipeline/local_prompt_pipeline.py

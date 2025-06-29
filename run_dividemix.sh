85252387ccc883d4f68fb1e7f40951ebdf5772c0
Daniel Kim <dhjkim2001@gmail.com>
2025-06-29 15:20:15 +0100

    Create run_dividemix.sh

---
+++ run_dividemix.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N eval_local_mistral
+#$ -cwd
+#$ -l h_rt=12:00:00
+#$ -l h_vmem=512G
+#$ -q gpu
+#$ -l gpu=1
+#$ -o log/output.log
+#$ -e log/error.log
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load anaconda
+
+conda activate /exports/eddie/scratch/s2017594/conda-envs/diss
+
+rm -rf log/error.log log/output.log
+
+python src/pipeline/dividemix_pipeline.py

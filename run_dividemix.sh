d24a8a1b61c7967fa2a7e958871e0004d93816f7
Daniel Kim <dhjkim2001@gmail.com>
2025-07-03 10:44:26 +0100

    Update run_dividemix.sh

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -12,6 +12,16 @@
 # Initialise the environment modules
 . /etc/profile.d/modules.sh
 
+ENV_FILE=".env"
+
+KEY1="HEAD_TYPE"
+VALUE1="linear"
+if grep -q "^$KEY1=" "$ENV_FILE"; then
+    sed -i "s|^$KEY1=.*|$KEY1=$VALUE1|" "$ENV_FILE"
+else
+    echo "$KEY1=$VALUE1" >> "$ENV_FILE"
+fi
+
 # Load Python
 module load anaconda
 

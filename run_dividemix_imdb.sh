a6fa284bb20eadd6406d0e3acee07d0f3f294b9d
Daniel Kim <dhjkim2001@gmail.com>
2025-07-03 11:57:30 +0100

    Create run_dividemix_imdb.sh

---
+++ run_dividemix_imdb.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N dividemix_imdb
+#$ -cwd
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=512G
+#$ -q gpu
+#$ -l gpu=1
+#$ -o log/output.log
+#$ -e log/error.log
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+grep -q '^HEAD_TYPE=' .env && \
+  sed -i'' 's/^HEAD_TYPE=.*/HEAD_TYPE=linear/' .env || \
+  echo 'HEAD_TYPE=linear' >> .env
+
+grep -q '^USE_IMDB=' .env && \
+  sed -i'' 's/^USE_IMDB=.*/USE_IMDB=True/' .env || \
+  echo 'USE_IMDB=True' >> .env
+
+# Load Python
+module load anaconda
+
+conda activate /exports/eddie/scratch/s2017594/conda-envs/diss
+
+python src/pipeline/dividemix_pipeline.py

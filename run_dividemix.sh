f98e879a1a79f28b6c729e5b79688292684a0dcf
Daniel Kim <dhjkim2001@gmail.com>
2025-07-15 09:55:56 +0100

    Update run_dividemix.sh

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_output_higher_threshold
+#$ -N dividemix_50_output
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_50_higher_threshold.log
-#$ -e log/error_dividemix_50_higher_threshold.log
+#$ -o log/output_dividemix_50.log
+#$ -e log/error_dividemix_50.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

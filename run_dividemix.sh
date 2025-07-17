b4805c9f2e025dd8894dc462990668bef917cbe9
Daniel Kim <dhjkim2001@gmail.com>
2025-07-17 15:42:37 +0100

    update env

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_output_warmup=5
+#$ -N dividemix_50_output_pen=0.5
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_50_warmup=5.log
-#$ -e log/error_dividemix_50_warmup=5.log
+#$ -o log/output_dividemix_50_pen=0.5.log
+#$ -e log/error_dividemix_50_pen=0.5.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

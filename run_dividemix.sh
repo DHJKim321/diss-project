f3285324670ab332df95c14a24ff8c525b4c4d55
Daniel Kim <dhjkim2001@gmail.com>
2025-07-17 21:11:44 +0100

    Update run_dividemix.sh

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_output_pen=0.5
+#$ -N dividemix_50_output_changed_order
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_50_pen=0.5.log
-#$ -e log/error_dividemix_50_pen=0.5.log
+#$ -o log/output_dividemix_changed_order.log
+#$ -e log/error_dividemix_changed_order.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

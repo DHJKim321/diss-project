19e8bee616de803e031686f2cb7d33179b74145d
Daniel Kim <dhjkim2001@gmail.com>
2025-07-18 14:51:17 +0100

    new env

--- run_dividemix_yahoo.sh
+++ run_dividemix_yahoo.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_yahoo
+#$ -N dividemix_yahoo_warmup=10
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_yahoo.log
-#$ -e log/error_dividemix_yahoo.log
+#$ -o log/output_dividemix_yahoo_warmup=10.log
+#$ -e log/error_dividemix_yahoo_warmup=10.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

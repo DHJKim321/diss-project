ffc18c453f4b968ee9faa93854f12639afb135bf
Daniel Kim <dhjkim2001@gmail.com>
2025-07-19 10:12:30 +0100

    Update run_dividemix.sh

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_output_freeze_encoder
+#$ -N dividemix_50_output_ramup=8
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_freeze_encoder.log
-#$ -e log/error_dividemix_freeze_encoder.log
+#$ -o log/output_dividemix_ramup=8.log
+#$ -e log/error_dividemix_ramup=8.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

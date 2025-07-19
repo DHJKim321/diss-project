eeda4848c607d92f277d3decfddc39ca05007b8f
Daniel Kim <dhjkim2001@gmail.com>
2025-07-19 18:45:31 +0100

    Update data_utils.py

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -120,7 +120,7 @@ def save_orig_noisy_loss_histogram(noisy_mask, raw_losses, epoch, model):
     plt.title(f"Epoch {epoch}: Loss Distribution (Model {model})")
     plt.legend()
     plt.tight_layout()
-    plt.savefig(f"src/data/images/loss/clean_noisy_loss_{epoch}_model_{model}.png")
+    plt.savefig(f"src/data/images/loss_no_lu/clean_noisy_loss_{epoch}_model_{model}.png")
     plt.close()
 
 def load_yahoo_train(file, file_path):

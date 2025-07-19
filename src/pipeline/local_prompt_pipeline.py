00cb23a2b8a0a3129974a64534fcb030ad1e48c6
Daniel Kim <dhjkim2001@gmail.com>
2025-07-19 22:32:01 +0100

    yahoo llm prompt

--- src/pipeline/local_prompt_pipeline.py
+++ src/pipeline/local_prompt_pipeline.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 from dotenv import load_dotenv
 
 from src.utils.llm_utils import *
-from src.utils.data_utils import load_test_data
+from src.utils.data_utils import load_test_data, load_yahoo_test
 from src.prompts.templates import *
 from src.utils.eval_utils import evaluate_model, save_evaluation, add_predictions_to_data
 
@@ -15,17 +15,23 @@ if __name__ == "__main__":
     test_file = os.getenv("TEST_FILE")
     test_path = os.getenv("TEST_DATA_PATH")
     save_path = os.getenv("DATA_SAVE_PATH")
+    use_yahoo = os.getenv("USE_YAHOO").lower() == 'true'
 
-    data = load_test_data(test_file, test_path)                         
+    if use_yahoo:
+        print("Using Yahoo test data")
+        data = load_yahoo_test(test_file, test_path)
+    else:
+        print("Using ShaPe test data")
+        data = load_test_data(test_file, test_path)                         
 
     pipeline_ = load_llama_model(model_path)
 
-    for _, template in enumerate([TEMPLATE_V3]):
-        print(f"Processing with template {3}")
-        updated_data = batch_process(pipeline_, template, data, new_col=f'predictions_{3}', num_posts=4, test_file=test_file, data_path=save_path, source_col='text')
+    for i, template in enumerate([TEMPLATE_V1, TEMPLATE_V2, TEMPLATE_V3]):
+        print(f"Processing with template {i+1}")
+        updated_data = batch_process(pipeline_, template, data, new_col=f'predictions_{i+1}', num_posts=4, test_file=test_file, data_path=save_path, source_col='text')
 
-        predictions = updated_data[f'predictions_{3}'].tolist()
+        predictions = updated_data[f'predictions_{i+1}'].tolist()
         labels = updated_data['label'].tolist()
         evaluations = evaluate_model(predictions, labels)
-        save_evaluation(evaluations, test_file, save_path, f'mistral_template_{3}')
-        add_predictions_to_data(updated_data, test_file, save_path, predictions, f'mistral_template_{3}', template=f"_{3}")
+        save_evaluation(evaluations, test_file, save_path, f'yahoo_mistral_template_{i+1}')
+        add_predictions_to_data(updated_data, test_file, save_path, predictions, f'yahoo_mistral_template_{i+1}', template=f"_{i+1}")

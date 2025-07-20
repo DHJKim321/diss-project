5402a3616e22f22e86d18809e3b787fd032300b1
Daniel Kim <dhjkim2001@gmail.com>
2025-07-20 10:21:02 +0100

    no Lu

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_warmup=10
+#$ -N dividemix_50_no_Lu
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_warmup=10.log
-#$ -e log/error_dividemix_warmup=10.log
+#$ -o log/output_dividemix_no_Lu.log
+#$ -e log/error_dividemix_no_Lu.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

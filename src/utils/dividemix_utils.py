a04a81da6b946eeca063f14b3638db09e0f3d2e0
Daniel Kim <dhjkim2001@gmail.com>
2025-07-20 17:11:06 +0100

    Commented out prints

--- src/utils/dividemix_utils.py
+++ src/utils/dividemix_utils.py
@@ -132,7 +132,7 @@ def train(epoch_no, model1, model2, optimizer, semiloss, labelled_loader, unlabe
         targets_u = mixed_labels[batch_size*2:]
 
         # Print label softmax for debugging
-        tqdm.write(f"Label Softmax (x): {torch.softmax(logits, dim=1).mean(dim=0).detach().cpu().numpy()}")
+        # tqdm.write(f"Label Softmax (x): {torch.softmax(logits, dim=1).mean(dim=0).detach().cpu().numpy()}")
 
         # Calculate individual losses
         Lx, Lu, lambda_u_val = semiloss(logits_x, targets_x, logits_u, targets_u, epoch_no, warmup_epochs)
@@ -164,7 +164,7 @@ def train(epoch_no, model1, model2, optimizer, semiloss, labelled_loader, unlabe
         # Get number of grad norms that are near zero
         # grad_norms = [p.grad.norm().item() for p in model1.parameters() if p.grad is not None]
         # near_zero_grad_count = sum(1 for norm in grad_norms if norm <= 1e-6)
-        tqdm.write(f"Epoch {epoch_no}, Batch {batch_idx+1}/{num_iter}, Lx {Lx.item():.4f}, Lu {lambda_u_val * Lu.item():.4f}, Penalty {penalty_val * penalty.item():.4f}, loss {loss.item():.4f}")
+        # tqdm.write(f"Epoch {epoch_no}, Batch {batch_idx+1}/{num_iter}, Lx {Lx.item():.4f}, Lu {lambda_u_val * Lu.item():.4f}, Penalty {penalty_val * penalty.item():.4f}, loss {loss.item():.4f}")
         return total_loss / count, total_Lx / count, total_Lu / count, total_penalty / count
 
 def eval_train(model, criterion, eval_loader, device='cuda'):

1e57e2abd48ac5e8cade7cda26a27d3dbbada1ad
Daniel Kim <dhjkim2001@gmail.com>
2025-07-20 16:22:51 +0100

    threshold to 0.7

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_aeda
+#$ -N dividemix_50_aeda_p=0.7
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_aeda.log
-#$ -e log/error_dividemix_aeda.log
+#$ -o log/output_dividemix_aeda_p=0.7.log
+#$ -e log/error_dividemix_aeda_p=0.7.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

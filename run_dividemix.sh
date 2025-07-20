b3af779837cefc596f2117ca558a18266e95dc6a
Daniel Kim <dhjkim2001@gmail.com>
2025-07-20 22:22:55 +0100

    Set p to 0.9

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_aeda_p=0.7_warmup=5
+#$ -N dividemix_50_aeda_p=0.9
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_aeda_p=0.7_warmup=5.log
-#$ -e log/error_dividemix_aeda_p=0.7_warmup=5.log
+#$ -o log/output_dividemix_aeda_p=0.9.log
+#$ -e log/error_dividemix_aeda_p=0.9.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

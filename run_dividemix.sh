0b2c8fd26ebbadcb492e3b1c529c185802684943
Daniel Kim <dhjkim2001@gmail.com>
2025-07-20 19:07:59 +0100

    set lambda u to 0

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_aeda_p=0.7_lambda_u=50
+#$ -N dividemix_50_aeda_p=0.7_lambda_u=0
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_aeda_p=0.7_lambda_u=50.log
-#$ -e log/error_dividemix_aeda_p=0.7_lambda_u=50.log
+#$ -o log/output_dividemix_aeda_p=0.7_lambda_u=0.log
+#$ -e log/error_dividemix_aeda_p=0.7_lambda_u=0.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh
--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -106,7 +106,7 @@ def save_orig_noisy_loss_histogram(noisy_mask, raw_losses, epoch, model):
     plt.title(f"Epoch {epoch}: Loss Distribution (Model {model})")
     plt.legend()
     plt.tight_layout()
-    plt.savefig(f"src/data/images/loss_p=0.7_lambda_u=50/clean_noisy_loss_{epoch}_model_{model}.png")
+    plt.savefig(f"src/data/images/loss_p=0.7_lambda_u=0/clean_noisy_loss_{epoch}_model_{model}.png")
     plt.close()
 
 def load_yahoo_train(file, file_path):

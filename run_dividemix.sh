0a9b34417e35043867934f6e0fa8a709f29d687b
Daniel Kim <dhjkim2001@gmail.com>
2025-07-22 18:39:27 +0100

    Testing

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_aedasyn_warmup=30_lr=1
+#$ -N dividemix_50_reddit
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_aedasyn_warmup=30_lr=1.log
-#$ -e log/error_dividemix_aedasyn_warmup=30_lr=1.log
+#$ -o log/output_dividemix_reddit.log
+#$ -e log/error_dividemix_reddit.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

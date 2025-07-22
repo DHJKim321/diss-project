0ed0c123e02cb4b7c6c420dde197bc682d5aa73d
Daniel Kim <dhjkim2001@gmail.com>
2025-07-22 20:38:05 +0100

    agnews running on colab

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -106,7 +106,7 @@ def save_orig_noisy_loss_histogram(noisy_mask, raw_losses, epoch, model):
     plt.title(f"Epoch {epoch}: Loss Distribution (Model {model})")
     plt.legend()
     plt.tight_layout()
-    plt.savefig(f"src/data/images/loss_reddit/clean_noisy_loss_{epoch}_model_{model}.png")
+    plt.savefig(f"src/data/images/loss_agnews_colab/clean_noisy_loss_{epoch}_model_{model}.png")
     plt.close()
 
 def load_yahoo_train(file, file_path):

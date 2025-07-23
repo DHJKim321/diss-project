1def01f448763bb2bf77c54186e588c6f18ceb4f
Daniel Kim <dhjkim2001@gmail.com>
2025-07-23 12:39:07 +0100

    Update run_dividemix.sh

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_50_reddit
+#$ -N dividemix
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_reddit.log
-#$ -e log/error_dividemix_reddit.log
+#$ -o log/output_dividemix.log
+#$ -e log/error_dividemix.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

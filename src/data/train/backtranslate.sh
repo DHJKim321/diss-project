98690a0018bca03c5357c8db5943761d0595b49b
Daniel Kim <dhjkim2001@gmail.com>
2025-07-25 16:22:24 +0100

    testing

---
+++ src/data/train/backtranslate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N backtranslate
+#$ -cwd
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=512G
+#$ -q gpu
+#$ -l gpu=1
+#$ -o log/output_backtranslate.log
+#$ -e log/error_backtranslate.log
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load anaconda
+
+conda activate /exports/eddie/scratch/s2017594/conda-envs/diss
+
+python src/data/train/backtranslate.py
+

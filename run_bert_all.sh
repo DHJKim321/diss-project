7c6ea6d59198fbe65cc57b20b412d6ce55a63efb
Daniel Kim <dhjkim2001@gmail.com>
2025-07-29 18:05:15 +0100

    80 baseline agnews

--- run_bert_all.sh
+++ run_bert_all.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N bert_baseline_ag_0.5
+#$ -N bert_baseline_ag_0.8
 #$ -cwd
 #$ -l h_rt=24:00:00
 #$ -l h_vmem=100G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_bert_ag_0.5.log
-#$ -e log/error_bert_ag_0.5.log
+#$ -o log/output_bert_ag_0.8.log
+#$ -e log/error_bert_ag_0.8.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh

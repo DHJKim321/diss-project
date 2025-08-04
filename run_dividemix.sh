e2f381d04b8299948a885b8ad822510accd5b4cc
Daniel Kim <dhjkim2001@gmail.com>
2025-08-04 10:17:59 +0100

    80

--- run_dividemix.sh
+++ run_dividemix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N dividemix_ag_0.9
+#$ -N dividemix_ag_0.8
 #$ -cwd
 #$ -l h_rt=48:00:00
 #$ -l h_vmem=512G
 #$ -q gpu
 #$ -l gpu=1
-#$ -o log/output_dividemix_ag_0.9.log
-#$ -e log/error_dividemix_ag_0.9.log
+#$ -o log/output_dividemix_ag_0.8.log
+#$ -e log/error_dividemix_ag_0.8.log
 
 # Initialise the environment modules
 . /etc/profile.d/modules.sh
